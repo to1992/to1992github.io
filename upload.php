f7277f607f34bddf1a8ecdf8ba41bf7847af5bfe
to1992 <46072297+to1992@users.noreply.github.com>
2018-12-23 04:17:24 +0900

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,25 +1,10 @@
-<!DOCTYPE html>
-<html lang="ja">
-<html>
-    <head>
-     <meta charset=utf-8">
-     <title>sample</title>
-    </head>
-    <body>
-        <?php
-        //↓uploadファイルの有り無し確認
-        if (is_uploaded_file($_FILES["upfile"]["tmp_name"])) {
-        //↓有効なファイルかどうかを検証し、問題なければ名前を変更しアップロード完了
-            if (move_uploaded_file($_FILES["upfile"]["tmp_name"], "picture/" . $_FILES["upfile"]["name"])) {
-            chmod("picture/" . $_FILES["upfile"]["name"], 0644);   //パーミッション設定
-            echo $_FILES["upfile"]["name"] . "をアップロードしました。";
-            } 
-            else {
-            echo "ファイルをアップロードできません。";
-            }   
-            } else {
-            echo "ファイルが選択されていません。";
-            }
-        ?>
-    </body>
-</html>
+<?php 
+  //ファイルの保存先
+  $upload = 'https://github.com/to1992/to1992github.io/tree/master/picture/'.$_FILES['file_upload']['name']; 
+  //アップロードが正しく完了したかチェック
+  if(move_uploaded_file($_FILES['file_upload']['tmp_name'], $upload)){
+    echo 'アップロード完了';
+  }else{
+    echo 'アップロード失敗'; 
+  }
+?>
